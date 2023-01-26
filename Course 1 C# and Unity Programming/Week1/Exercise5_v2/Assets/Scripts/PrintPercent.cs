22043f35f1eeb6b2edf0af0337e9619efd3d7664
Memory <59786843+Adomnr@users.noreply.github.com>
2023-01-26 18:03:29 +0500

    Add files via upload

---
+++ Course 1 C# and Unity Programming/Week1/Exercise5_v2/Assets/Scripts/PrintPercent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintPercent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        const int MaxScore = 100;
+        int score = 50;
+        float percent = ((float) score / MaxScore) *100;
+        Debug.Log(percent);
+    }
+}
